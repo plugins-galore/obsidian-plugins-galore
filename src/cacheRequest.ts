fbc93e1a98e5769a1f4497f42ab7452d342b7f0f
dylan <>
2022-04-20 20:38:56 -0700

    fix cacheRequest function

--- src/cacheRequest.ts
+++ src/cacheRequest.ts
@@ -4,11 +4,11 @@ const cache = [];
 
 const cacheRequest = async (req) => {
 	const filteredCache = cache.filter(item => item.req === JSON.stringify(req));
-	if (!filteredCache.length) {
-		return filteredCache[0];
+	if (filteredCache.length) {
+		return filteredCache[0].res;
 	}
 	const res = await request(req);
-	cache.push({req, res});
+	cache.push({req: JSON.stringify(req), res});
 	return res;
 }
 
